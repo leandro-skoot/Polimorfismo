6ee078ab035e6ecc435c408dcda6182f96384c57
Leandro Silva <leandro.skoot@gmail.com>
2022-01-23 14:17:13 -0300

    Exercicios de Polimorfismo!

---
+++ Polimorfismo/Entities/OutSorceEmployee.cs
@@ -0,0 +1,24 @@
+﻿namespace Polimorfismo.Entities
+{
+    class OutSorceEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutSorceEmployee()
+        {
+        }
+
+        public OutSorceEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + AdditionalCharge * 1.1;
+        }
+    }
+
+
+}
---
+++ Polimorfismo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Polimorfismo.Entities;
+using System.Globalization;
+
+namespace Polimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:",i );
+                Console.Write("Outsourced(y / n) ? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hour = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (ch == 'y')
+                {
+                    Console.WriteLine("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutSorceEmployee(name, hour, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hour, valuePerHour));
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
